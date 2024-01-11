4e5016a7dba60f0b4dfaa85fd4aad4c886e57634
isko02 <iskren3009@abv.bg>
2024-01-11 14:07:07 +0200

    Containerizing the website with Docker.

--- Bulky.Utility/SD.cs
+++ Bulky.Utility/SD.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -29,7 +30,7 @@ namespace BulkyBook.Utility
         public const string SessionCart = "SessionShoppingCart";
 
 
-
+        
 
 
 
